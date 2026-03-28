67980a8b14f7a125079c3b8d1d02a10ed6567612
Lorene_Valencia <ladymirene@proton.me>
2026-03-28 08:01:45 +0800

    Iteration 6: Other Optimization & tests

--- resource-api/Dtos/CmsDtos.cs
+++ resource-api/Dtos/CmsDtos.cs
@@ -2,48 +2,137 @@ using System.ComponentModel.DataAnnotations;
 
 namespace resource_api.Dtos
 {
-    public record CreateTagRequest(
-        [property: Required, StringLength(64, MinimumLength = 2)] string Name
-    );
+    public class CreateTagRequest
+    {
+        [Required]
+        [StringLength(64, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+    }
 
-    public record CreateImageFromUrlRequest(
-        [property: Required, Url, StringLength(1000)] string Url,
-        [property: StringLength(120)] string? Name
-    );
+    public class CreateImageFromUrlRequest
+    {
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Url { get; set; } = string.Empty;
 
-    public record AddImageTagRequest(
-        [property: Range(1, int.MaxValue)] int TagId
-    );
+        [StringLength(120)]
+        public string? Name { get; set; }
+    }
 
-    public record TagDto(int Id, string Name);
+    public class AddImageTagRequest
+    {
+        [Range(1, int.MaxValue)]
+        public int TagId { get; set; }
+    }
 
-    public record CmsImageDto(int Id, string Url, string? Name, DateTime CreatedAtUtc, List<TagDto> Tags);
+    public class TagDto
+    {
+        public TagDto(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
 
-    public record CreatePackRequest(
-        [property: Required, StringLength(120, MinimumLength = 2)] string Name,
-        [property: StringLength(400)] string Description,
-        [property: Range(1, 5)] int Difficulty
-    );
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 
-    public record UpdatePackRequest(
-        [property: Required, StringLength(120, MinimumLength = 2)] string Name,
-        [property: StringLength(400)] string Description,
-        [property: Range(1, 5)] int Difficulty
-    );
+    public class CmsImageDto
+    {
+        public CmsImageDto(int id, string url, string? name, DateTime createdAtUtc, List<TagDto> tags)
+        {
+            Id = id;
+            Url = url;
+            Name = name;
+            CreatedAtUtc = createdAtUtc;
+            Tags = tags;
+        }
 
-    public record CreatePuzzleRequest(
-        [property: Required, StringLength(64, MinimumLength = 1)] string Answer,
-        [property: Required, Url, StringLength(1000)] string Image1Url,
-        [property: Required, Url, StringLength(1000)] string Image2Url,
-        [property: Required, Url, StringLength(1000)] string Image3Url,
-        [property: Required, Url, StringLength(1000)] string Image4Url
-    );
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public List<TagDto> Tags { get; set; }
+    }
 
-    public record UpdatePuzzleRequest(
-        [property: Required, StringLength(64, MinimumLength = 1)] string Answer,
-        [property: Required, Url, StringLength(1000)] string Image1Url,
-        [property: Required, Url, StringLength(1000)] string Image2Url,
-        [property: Required, Url, StringLength(1000)] string Image3Url,
-        [property: Required, Url, StringLength(1000)] string Image4Url
-    );
+    public class CreatePackRequest
+    {
+        [Required]
+        [StringLength(120, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(400)]
+        public string Description { get; set; } = string.Empty;
+
+        [Range(1, 5)]
+        public int Difficulty { get; set; }
+    }
+
+    public class UpdatePackRequest
+    {
+        [Required]
+        [StringLength(120, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(400)]
+        public string Description { get; set; } = string.Empty;
+
+        [Range(1, 5)]
+        public int Difficulty { get; set; }
+    }
+
+    public class CreatePuzzleRequest
+    {
+        [Required]
+        [StringLength(64, MinimumLength = 1)]
+        public string Answer { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image1Url { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image2Url { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image3Url { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image4Url { get; set; } = string.Empty;
+    }
+
+    public class UpdatePuzzleRequest
+    {
+        [Required]
+        [StringLength(64, MinimumLength = 1)]
+        public string Answer { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image1Url { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image2Url { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image3Url { get; set; } = string.Empty;
+
+        [Required]
+        [Url]
+        [StringLength(1000)]
+        public string Image4Url { get; set; } = string.Empty;
+    }
 }
